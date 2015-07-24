36412974c720801404dd1a8fa4dedb2e734218a4
Rene Nederhand <rene@cogfor.com>
2015-07-24 01:00:00 +0000

    this doesn't really make things faster, but I tried

--- superpy.py
+++ superpy.py
@@ -1012,3 +1012,8 @@ output = asarray(DataReader('GDPC1', 'fred',
     y = np.asarray(y, dtype=np.int32)
     # the OpenCV wrapper happy:
     # If a out_dir is given, set it:
+
+        return None, value
+        return None, None, mesg
+    return np.asarray(pressure), aver, mesg
+
