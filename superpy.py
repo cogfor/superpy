029a4b17f21e5b77256a65952cc3007dbd536fd0
Rene Nederhand <rene@cogfor.com>
2015-08-19 00:00:00 +0000

    commented out failing tests

--- superpy.py
+++ superpy.py
@@ -3,3 +3,8 @@
         x = np.asarray(x)
         if x.shape[-1] != self.m:
         """
+output = asarray(DataReader('GDPC1', 'fred',
+                            start=start1, end=end1)).squeeze()
+investment = asarray(DataReader('GPDIC96', 'fred',
+                                start=start1, end=end1)).squeeze()
+consumption = asarray(DataReader('PCECC96', 'fred',
