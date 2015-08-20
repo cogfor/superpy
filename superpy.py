5858acf3d2698061c5b29a816066c311191d089c
Rene Nederhand <rene@cogfor.com>
2015-08-20 00:00:00 +0000

    That's just how I roll

--- superpy.py
+++ superpy.py
@@ -8,3 +8,8 @@ output = asarray(DataReader('GDPC1', 'fred',
 investment = asarray(DataReader('GPDIC96', 'fred',
                                 start=start1, end=end1)).squeeze()
 consumption = asarray(DataReader('PCECC96', 'fred',
+        self.A = asarray([[1.0, 0.0, 1.0], [0.0, 1.0, -4.0]])
+        self.b = asarray([-1.0, 0.0])
+        self.bounds = ((None, None), (0.1, 2.9), (0.7, None))
+from numpy.testing import *
+from numpy import (arange, asarray, empty, float64, zeros)
